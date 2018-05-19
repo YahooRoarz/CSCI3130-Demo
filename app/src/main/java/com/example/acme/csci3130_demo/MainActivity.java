1f2aa5d5002228dc7548745c6a8c98630cd9cc96
YahooRoarz <39277274+YahooRoarz@users.noreply.github.com>
2018-05-20 00:18:47 +0800

    a

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.EditText;
 import android.os.Bundle;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button Mybutton = (Button) findViewById(R.id.Mybutton);
+        final TextView Show = (TextView)findViewById(R.id.show);
+        final EditText text = (EditText) findViewById(R.id.text);
+
+        Mybutton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Show.setText(text.getText());
+            }
+        });
+
+
     }
 }
